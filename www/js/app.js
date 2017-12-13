76ddfd7ddc17e5f2678701c94e4bd04e55ba1543
Muhammad Ansharullah <ansorkazama@gmail.com>
2017-12-13 21:45:41 +0700

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,181 @@
+angular.module("aplikasi_e_absensi", ["ngCordova","ionic","ionMdInput","ionic-material","ion-datetime-picker","ionic.rating","utf8-base64","angular-md5","chart.js","pascalprecht.translate","aplikasi_e_absensi.controllers", "aplikasi_e_absensi.services"])
+	.run(function($ionicPlatform,$window,$interval,$timeout,$ionicHistory,$ionicPopup,$state,$rootScope){
+
+		$rootScope.appName = "Aplikasi E-Absensi" ;
+		$rootScope.appLogo = "data/images/header/logo.png" ;
+		$rootScope.appVersion = "1.0" ;
+		$rootScope.headerShrink = false ;
+
+		$ionicPlatform.ready(function() {
+			//required: cordova plugin add ionic-plugin-keyboard --save
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			//required: cordova plugin add cordova-plugin-statusbar --save
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+
+		});
+		$ionicPlatform.registerBackButtonAction(function (e){
+			if($ionicHistory.backView()){
+				$ionicHistory.goBack();
+			}else{
+				var confirmPopup = $ionicPopup.confirm({
+					title: "Confirm Exit",
+					template: "Are you sure you want to exit?"
+				});
+				confirmPopup.then(function (close){
+					if(close){
+						ionic.Platform.exitApp();
+					}
+				});
+			}
+			e.preventDefault();
+			return false;
+		},101);
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+	.filter("trustUrl", function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	})
+
+	.filter("trustJs", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsJs(text);
+		};
+	}])
+
+	.filter("strExplode", function() {
+		return function($string,$delimiter) {
+			if(!$string.length ) return;
+			var $_delimiter = $delimiter || "|";
+			return $string.split($_delimiter);
+		};
+	})
+
+	.filter("strDate", function(){
+		return function (input) {
+			return new Date(input);
+		}
+	})
+	.filter("strHTML", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+	.filter("strEscape",function(){
+		return window.encodeURIComponent;
+	})
+	.filter("strUnscape", ["$sce", function($sce) {
+		var div = document.createElement("div");
+		return function(text) {
+			div.innerHTML = text;
+			return $sce.trustAsHtml(div.textContent);
+		};
+	}])
+
+	.filter("objLabel", function(){
+		return function (obj) {
+			var new_item = [];
+			angular.forEach(obj, function(child) {
+				new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v,l) {
+					if (indeks !== 0) {
+					new_item.push(l);
+				}
+				indeks++;
+				});
+			});
+			return new_item;
+		}
+	})
+	.filter("objArray", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if (indeks !== 0){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+
+.config(["$translateProvider", function ($translateProvider){
+	$translateProvider.preferredLanguage("en-us");
+	$translateProvider.useStaticFilesLoader({
+		prefix: "translations/",
+		suffix: ".json"
+	});
+}])
+
+
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider,$httpProvider,$ionicConfigProvider){
+	try{
+		// Domain Whitelist
+		$sceDelegateProvider.resourceUrlWhitelist([
+			"self",
+			new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?w3schools\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?maps\.googleapis\.com/.+$'),
+		]);
+	}catch(err){
+		console.log("%cerror: %cdomain whitelist","color:blue;font-size:16px;","color:red;font-size:16px;");
+	}
+	$stateProvider
+	.state("aplikasi_e_absensi",{
+		url: "/aplikasi_e_absensi",
+			abstract: true,
+			templateUrl: "templates/aplikasi_e_absensi-side_menus.html",
+			controller: "side_menusCtrl",
+	})
+
+	.state("aplikasi_e_absensi.about_us", {
+		url: "/about_us",
+		views: {
+			"aplikasi_e_absensi-side_menus" : {
+						templateUrl:"templates/aplikasi_e_absensi-about_us.html",
+						controller: "about_usCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("aplikasi_e_absensi.dashboard", {
+		url: "/dashboard",
+		views: {
+			"aplikasi_e_absensi-side_menus" : {
+						templateUrl:"templates/aplikasi_e_absensi-dashboard.html",
+						controller: "dashboardCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	$urlRouterProvider.otherwise("/aplikasi_e_absensi/dashboard");
+});
